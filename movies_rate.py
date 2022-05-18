42666d8f5777379645a7c1f69dcdbd1acd37f48b
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-18 11:55:13 +0300

    Update movies_rate.py

--- movies_rate.py
+++ movies_rate.py
@@ -3,7 +3,7 @@ rate = 3
 popularity_score = 72.65
 if rate >= 4 or popularity_score > 80:
     print("Highly recommended") 
-elif rate == 3 or popularity_score > 70:
+elif rate >= 3 or popularity_score > 70:
     print("I recommended it . It is good")     
 elif rate <= 2 or popularity_score > 60:
     print("You should check it out!")
